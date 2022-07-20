6e4b02b8ab7f3b39f86667e2bf5302ef9ff7efa7
Dmitry Fuks <difuks@gmail.com>
2022-07-20 16:11:25 +0300

    Test

---
+++ .github/actions/get-release-info/src/index.js
@@ -0,0 +1,27 @@
+import core from '@actions/core';
+import github from '@actions/github';
+
+const getReleaseInfo = (tag) => {
+  const githubToken = core.getInput('github_token');
+
+  const octokit = github.getOctokit(githubToken);
+
+  return octokit.rest.repos.getRelease({
+    ...github.context.repo,
+    release_id: Number(tag),
+  })
+}
+
+(async () => {
+  const tag = core.getInput('tag');
+
+  const { data: releaseInfo } = await getReleaseInfo(tag);
+
+  core.setOutput('body', releaseInfo.body);
+  core.setOutput('name', releaseInfo.name);
+  core.setOutput('tag_name', releaseInfo.tag_name);
+  core.setOutput('url', releaseInfo.html_url);
+  core.setOutput('author_login', releaseInfo.author.login);
+  core.setOutput('author_avatar', releaseInfo.author.avatar_url);
+  core.setOutput('author_url', releaseInfo.author.html_url);
+})()
